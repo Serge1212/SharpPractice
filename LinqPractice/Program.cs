c18bb00f34c8de2d5155ea396f6c186a55a77fb4
Serge <sanichace@gmail.com>
2020-06-14 19:00:52 +0300

    Initial check-in of module LinqPractice

---
+++ LinqPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter num: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            bool Denis = num > 100 ? true : false;
+
+            if (Denis)
+                Console.WriteLine("This number is bigger than 100");
+            else if(!Denis) Console.WriteLine("This number is less than 100");
+        }
+    }
+}
